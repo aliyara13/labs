1845b898f0b97175133375e144f14b2b4644c062
Aliya Razakova <razakova384@gmail.com>
2024-01-24 20:20:41 +0600

    initial commit

---
+++ lab1/variables.py
@@ -0,0 +1,54 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       
+x = "Sally" 
+print(x)
+
+x = str(3)    
+y = int(3)    
+z = float(3)  
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+
+
+# Exercise 1
+carname = "Volvo"
+
+# Exercise 2
+x = 50
+
+# Exercise 3
+x = 5
+y = 10
+print(x + y)
+
+# Exercise 4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# Exercise 5
+x, y, z = "Orange", "Banana", "Cherry"
+
+# Exercise 6
+x = y = z = "Orange"
+
+# Exercise 7
+def myfunc():
+    global x
+    x = "fantastic"
+
